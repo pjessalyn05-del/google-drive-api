6a4daba03debf0e8788053a4cd6064c6c772ecb9
Ernie Mamaril Roble <126392006+iErdzy@users.noreply.github.com>
2025-12-26 03:18:18 +0800

    Update ACCESS_TOKEN in config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-// Store your credentials here. 
-// DO NOT commit this file to GitHub if your repo is public.
 const CONFIG = {
-    // API_KEY: 'YOUR_GOOGLE_API_KEY',
     CLIENT_ID: '392138468686-bf855t5vq59id8t56qakfvqtpkc72m9q.apps.googleusercontent.com',
-    ACCESS_TOKEN: 'ya29.a0Aa7pCA8ujNp2s1G1av8R5jlAZdHwdjTxl05ImlLIwRK0KEz35QJxo7lQqHufPj5CXw4vOPqCOSNlH_567O4usVNZZ1bzoSkSxDTbfQ_X0xhjfVJAYOl_66otYESAP3vyfjEXqaBv0-yfTj0oGM16PIsHVX25hUW-HD2HlP1dR8O0XSZoDolyWTwTpZs8j3fu4JZzhfQaCgYKAegSARMSFQHGX2MiTU6tKNXYuOAWdOI5lq_Glg0206', // Obtained from OAuth Playground
+    ACCESS_TOKEN: 'YOUR_GOOGLE_API_KEY', 
     BASE_URL: 'https://www.googleapis.com/drive/v3/files'
 };
+
 
 export default CONFIG;
